49c4d6fded9b43d8b1ce30b9483c9528d7006f1e
anbako <anbakokaban@gmail.com>
2020-07-30 16:06:45 +0900

    Update discordbot.py
    相談と情報交換カテゴリあげ機能

--- discordbot.py
+++ discordbot.py
@@ -12,9 +12,15 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.channel.category_id == 630802817522728960:
+        await message.channel.edit(position=0)
 
 bot.run(token)
