57ec491b6eb1bf149be1484ee8c04598f013b2b9
anbako <anbakokaban@gmail.com>
2020-07-31 16:58:39 +0900

    command削除
    コマンド削除

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,28 @@
-from discord.ext import commands
 import os
 import traceback
 import discord
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+# 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+    # メッセージが相談と情報交換カテゴリだったら上げる
     if message.channel.category_id == 630802817522728960:
         await message.channel.edit(position=0)
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 bot.run(token)
